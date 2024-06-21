cd6a967d4aea6d1f3cfc4dd4d540cea1c944122b
Romina Florencia Ramos <romi859@gmail.com>
2024-06-20 23:29:03 -0300

    Ejercicio 3, falta adicional

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -1,3 +1,4 @@
+// Clase que representa una moneda con un código y un nombre
 class Currency {
     constructor(code, name) {
         this.code = code;
@@ -5,12 +6,67 @@ class Currency {
     }
 }
 
+// Clase que maneja la conversión de monedas usando API externa
 class CurrencyConverter {
-    constructor() {}
+    constructor(apiUrl) {
+        this.apiUrl = apiUrl;
+        this.currencies = [];
+    }
 
-    getCurrencies(apiUrl) {}
+    //Método asíncrono para obtener la lista de códigos de monedas disponibles`
+    async getCurrencies() {
+        try {
+            // Realiza una petición GET a la API para obtener la lista de monedas
+            const response = await fetch(`${this.apiUrl}/currencies`);
 
-    convertCurrency(amount, fromCurrency, toCurrency) {}
+            if (!response.ok) {
+                throw new Error(`Error al cargar las monedas: ${response.statusText}`);
+            }
+
+            //Convierte la respuesta a JSON
+            const data = await response.json();
+
+            // Recorre el objeto data y crea instancias de Currency para cada moneda
+            for (const code in data) {
+                const currency = new Currency(code, data[code]);
+                this.currencies.push(currency);// Agrega la moneda a la lista de monedas disponibles
+            }
+            
+        } catch (error) {
+            console.error('Error al obtener las monedas:', error);
+        }
+    }
+
+
+    /*getCurrencies(apiUrl) {}*/
+
+    //Método asíncrono para realizar la conversión de una moneda a otra
+    async convertCurrency(amount, fromCurrency, toCurrency) {   // monto / origen / destino
+
+        if (fromCurrency.code === toCurrency.code) {
+            return amount; // Si son la misma moneda, retorna el monto original
+        }
+
+        try {
+
+            // Realiza una petición GET al endpoint /latest con los parámetros de conversión
+            const response = await fetch(`${this.apiUrl}/latest?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
+
+            if (!response.ok) {
+                throw new Error(`Error al realizar la conversión: ${response.statusText}`);
+            }
+
+            //Convierte la respuesta a JSON
+            const data = await response.json(); 
+            return data.rates[toCurrency.code]; // retorno el monto convertido
+
+        } catch (error) {
+            console.error('Error al convertir moneda:', error); 
+            return null;
+        }
+    }
+
+    
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
